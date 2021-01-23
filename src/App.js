6bf652a226d4c05ee44c232669a38ea4a7a42c3d
koderoom <koderoom.dev@gmail.com>
2021-01-23 10:40:09 +0530

    sharing information in nseted compnent hierarchy.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,49 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(100);
+  const [title, setTitle] = useState("Project Family Man.");
+
   return (
     <div>
-      <div>Parent Component</div>
-      <ChildA />
+      <div>Parent Component {counter}</div>
+      <ChildA counter={counter} />
     </div>
   );
 }
 
-function ChildA() {
+function ChildA(props) {
   return (
     <div>
-      <div>Child A</div>
-      <ChildB />
+      <div>Child A {props.counter}</div>
+      <ChildB counter={props.counter} />
     </div>
   );
 }
 
-function ChildB() {
+function ChildB(props) {
   return (
     <div>
-      <div>Child B</div>
-      <ChildC />
+      <div>Child B {props.counter}</div>
+      <ChildC counter={props.counter} />
     </div>
   );
 }
 
-function ChildC() {
+function ChildC(props) {
   return (
     <div>
-      <div>Child C</div>
+      <div>Child C {props.counter}</div>
+      <ChildD counter={props.counter} />
+    </div>
+  );
+}
+
+function ChildD(props) {
+  return (
+    <div>
+      <div>Child D {props.counter}</div>
     </div>
   );
 }
