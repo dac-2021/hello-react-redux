304965afea05983b180f5562793a3ac7bffb2615
koderoom <koderoom.dev@gmail.com>
2021-01-23 10:25:19 +0530

    hierarchy component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import "./App.css";
 
 function App() {
-  return <div className="bg-primary">Hello React Redux</div>;
+  return (
+    <div>
+      <div>Parent Component</div>
+      <ChildA />
+    </div>
+  );
+}
+
+function ChildA() {
+  return (
+    <div>
+      <div>Child A</div>
+      <ChildB />
+    </div>
+  );
+}
+
+function ChildB() {
+  return (
+    <div>
+      <div>Child B</div>
+      <ChildC />
+    </div>
+  );
+}
+
+function ChildC() {
+  return (
+    <div>
+      <div>Child C</div>
+    </div>
+  );
 }
 
 export default App;
